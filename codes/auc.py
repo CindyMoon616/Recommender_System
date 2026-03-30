d989d1af24e245de6f6b22fe0448eda8985b8d69
Cindy Moon <1084397300@qq.com>
2026-03-30 21:56:13 +0800

    0330.2

--- codes/auc.py
+++ codes/auc.py
@@ -12,3 +12,36 @@ def AUC(y_scores,y_true):
         if sorted_labels[i]==1:
             rank_sum += i
     auc = (rank_sum-pos_cnt*(pos_cnt-1)/2)/(pos_cnt*neg_cnt)
+
+def AUC_with_ties(y_scores, y_true):
+    # 创建(score, label, original_index)的元组
+    data = [(y_scores[i], y_true[i], i) for i in range(len(y_scores))]
+    # 按分数排序，分数相同时保持稳定
+    data.sort(key=lambda x: x[0])
+    
+    # 处理相同分数的平均秩次
+    n = len(data)
+    ranks = [0] * n
+    i = 0
+    while i < n:
+        j = i
+        # 找到所有相同分数的样本
+        while j < n and data[j][0] == data[i][0]:
+            j += 1
+        # 计算平均秩次
+        avg_rank = (i + j - 1) / 2.0
+        for k in range(i, j):
+            ranks[k] = avg_rank
+        i = j
+    
+    # 计算正样本的秩次和
+    rank_sum = 0
+    pos_cnt = 0
+    for k in range(n):
+        if data[k][1] == 1:  # 正样本
+            rank_sum += ranks[k]
+            pos_cnt += 1
+    
+    neg_cnt = n - pos_cnt
+    auc = (rank_sum - pos_cnt * (pos_cnt - 1) / 2) / (pos_cnt * neg_cnt)
+    return auc
